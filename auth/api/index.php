ccc6fa24f311ccf2b964373033c40e32864097df
danayaher <danayaher@alum.us.es>
2014-11-09 20:50:52 +0100

    API: añadidas funciones relacionadas con tokens
    Se han añadido a la API los métodos checkToken y checkTokenUser, que
    respectivamente comprueban si un token es válido y si un token es válido
    para un usuario.

--- auth/api/index.php
+++ auth/api/index.php
@@ -1,5 +1,6 @@
 <?php
 	include_once "../database.php";
+	include_once "../auth.php";
 
 	if(!isset($_GET['method']) || $_GET['method'] == ""){
 		badRequest();
@@ -7,6 +8,20 @@
 		switch ($_GET['method']) {
 			case 'getUsers':
 				getUsers();
+				break;
+			case 'checkToken':
+				if(!isset($_GET['token'])){
+					badRequest();
+				}else{
+					checkToken($_GET['token']);
+				}
+				break;
+			case 'checkTokenUser':
+				if(!isset($_GET['token']) || !isset($_GET['user'])){
+					badRequest();
+				}else{
+					checkTokenUser($_GET['token'], $_GET['user']);
+				}
 				break;
 			default:
 				badRequest();
@@ -17,7 +32,7 @@
 	function badRequest(){
 		header('HTTP/1.1 400 Bad Request');
 
-		echo "Bad Request. This method doesn't exists";
+		echo "Bad Request. This method doesn't exists or the necessary parameters weren't provided";
 	}
 
 	function getUsers(){
@@ -26,5 +41,17 @@
 		$users = json_encode(getAllUsers());
 
 		echo $users;
+	}
+
+	function checkToken($token){
+		$result['valid']=tokenIsCorrect($token);
+
+		echo json_encode($result);
+	}
+
+	function checkTokenUser($token, $user){
+		$result['valid']=checkUserToken($token, $user);
+
+		echo json_encode($result);
 	}
 ?>
