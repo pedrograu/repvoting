c1b774f71fee33cb00f5b81f43b21ff63ffa285f
alesanmed <alesanchezmedina@gmail.com>
2014-11-20 21:32:52 +0100

    BUGFIX: arreglado una restricción de campo en el script y nombres de tablas y campos.
    Se ha modificado la longitud máxima del campo "password" ya que estaba limitada a 30 caracteres y el algoritmo MD5 cifra a 32.
    Se han modificado las contraseñas de los usuarios por defecto para que estén cifradas en MD5, ya que el archivo de login comprueba la que pasa el usuario, cifrada.
    Se ha modificado el nombre de la tabla y de los campos de la misma para que se corresponda con los campos a los que se hacía referencia en los ficheros de la aplicación creados con anterioridad (auth.php y database.php)

--- auth/auth_DB_script.sql
+++ auth/auth_DB_script.sql
@@ -1,17 +1,17 @@
 CREATE DATABASE IF NOT EXISTS egcdb;
 USE egcdb;
-CREATE TABLE IF NOT EXISTS user(
+CREATE TABLE IF NOT EXISTS USERS(
 	username 	VARCHAR(30) NOT NULL,
-	password 	VARCHAR(30) NOT NULL,
+	password 	VARCHAR(32) NOT NULL,
 	email 	VARCHAR(30) NOT NULL,
 	genero 	ENUM('Femenino','Masculino') NOT NULL,
 	comunidad_autonoma 	ENUM('Andalucia','Murcia','Extremadura','Castilla la Mancha','Comunidad Valenciana','Madrid','Castilla y Leon','Aragon','Cataluña','La Rioja','Galicia','Asturias','Cantabria','Pais Vasco','Navarra')NOT NULL,
 	edad 	TINYINT NOT NULL);
 
-INSERT INTO user VALUE('danayaher','danayaher1','danayaher@alum.us.es','Masculino','Andalucia','22');
-INSERT INTO user VALUE('dandelea','dandelea1','dandelea@alum.us.es','Masculino','Andalucia','22');
-INSERT INTO user VALUE('fidmazdel','fidmazdel1','fidmazdel@alum.us.es','Masculino','Andalucia','24');
-INSERT INTO user VALUE('juarolsal','juarolsal1','juarolsal@alum.us.es','Masculino','Andalucia','22');
-INSERT INTO user VALUE('alesanmed','alesanmed1','alesanmed@alum.us.es','Masculino','Andalucia','22');
-INSERT INTO user VALUE('juacaslop','juacaslop1','juacaslop@alum.us.es','Masculino','Andalucia','27');
+INSERT INTO USERS VALUE('danayaher','7a1b688bc2bb3cc02e0d55c4e0188fb0','danayaher@alum.us.es','Masculino','Andalucia','22');
+INSERT INTO USERS VALUE('dandelea','9cf23ad866a1953b3dd93c80f595ea11','dandelea@alum.us.es','Masculino','Andalucia','22');
+INSERT INTO USERS VALUE('fidmazdel','b746ac06bca08e9c60f1e67f9a978253','fidmazdel@alum.us.es','Masculino','Andalucia','24');
+INSERT INTO USERS VALUE('juarolsal','9f1644a43dbfbaf05fda6ec642430b4d','juarolsal@alum.us.es','Masculino','Andalucia','22');
+INSERT INTO USERS VALUE('alesanmed','2c678f01c9222350776420037a69a1db','alesanmed@alum.us.es','Masculino','Andalucia','22');
+INSERT INTO USERS VALUE('juacaslop','f8e70dcaaf443f4fadd34959adaca9d2','juacaslop@alum.us.es','Masculino','Andalucia','27');
 COMMIT;
