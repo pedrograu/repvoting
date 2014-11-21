e355dc2f1335a7723594e12d5ee811bd4143ca3f
Daniel Utrera <dani.rota2@gmail.com>
2014-11-21 13:52:18 +0100

    Subido codigo actual de verificación y librería usada de conexión aMySQL

---
+++ verification/main/AuthorityImpl.java
@@ -0,0 +1,75 @@
+package main;
+
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.SecureRandom;
+import java.util.HashMap;
+
+import javax.xml.bind.DatatypeConverter;
+
+
+
+
+public class AuthorityImpl implements Authority{
+
+	private static HashMap<String, KeyPair> database = new HashMap<String, KeyPair>();
+	
+	public boolean postKey(String id) {
+		
+		try {
+
+			
+			 KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
+			
+			 SecureRandom random = SecureRandom.getInstance("SHA1PRNG","SUN");
+			 keyGen.initialize(1024, random);
+			
+			 KeyPair pair = keyGen.generateKeyPair();
+			
+			 database.put(id, pair);
+			 
+			 String publicKey = DatatypeConverter.printBase64Binary(pair.getPublic().getEncoded());
+			 String privateKey= DatatypeConverter.printBase64Binary(pair.getPrivate().getEncoded());
+			 
+			 
+			 DataBaseManager dbm=new DataBaseManager();
+			 dbm.saveKeys(id, publicKey, privateKey);
+			
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	
+		
+		return false;
+	}
+
+	
+	public String getPublicKey(String id) {
+		DataBaseManager dbm=new DataBaseManager();
+		
+		return dbm.getPublicKey(id);
+	}
+
+	
+	public String getPrivateKey(String id) {
+
+		DataBaseManager dbm=new DataBaseManager();
+		
+		
+		return dbm.getPrivateKey(id);
+	}
+
+	
+	public boolean checkVote(String votoCifrado, String id) {
+
+		/*TODO*/
+		//Get the key
+		//String key = getPublicKey(id);
+		
+		return false;
+
+	}
+
+}
