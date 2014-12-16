753ad855c2c83960b919d6a2f1413c6bb58312ec
José María Caballero Alba <caballeroalba@gmail.com>
2014-12-16 21:14:02 +0100

    añadido script instalación y manual

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "Introducir contraseña root  copia esta si es la de dp --> V3rY=$tR0nG=P@$$w0rd$"
+mysql -u root -p < DeliberationsScript.sql
+cd Deliberations
+
+mvn clean install
+
+echo "borrando Deliberations (si estaba) de Tomcat"
+rm -r -f /var/lib/tomcat7/webapps/Deliberations
+rm /var/lib/tomcat/webapps/Deliberations.war
+
+echo "copiando el war"
+cp target/Deliberations-1.80.war /var/lib/tomcat7/webapps/Deliberations.war
+
+echo "reiniciando tomcat"
+service tomcat restart
+
